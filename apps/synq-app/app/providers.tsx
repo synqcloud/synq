5354781307918f21cdac9a117c597d2d391a1b20
Telmo Beroiz Bilbao <iamtelmo@proton.me>
2025-10-03 12:41:32 +0200

    chore(app): add posthog measures

--- apps/synq-app/app/providers.tsx
+++ apps/synq-app/app/providers.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import posthog from "posthog-js";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
+import { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { TooltipProvider } from "@synq/ui/component";
@@ -16,16 +18,28 @@ export function SynqProviders({
 }: SynqProvidersProps) {
   const [queryClient] = useState(() => new QueryClient());
 
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: "/ingest",
+      ui_host: "https://us.posthog.com",
+      defaults: '2025-05-24',
+      capture_exceptions: true,
+      debug: process.env.NODE_ENV === "development",
+    });
+  }, []);
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <NextThemesProvider
-        attribute="class"
-        defaultTheme={initialTheme}
-        enableSystem
-        disableTransitionOnChange
-      >
-        <TooltipProvider>{children}</TooltipProvider>
-      </NextThemesProvider>
-    </QueryClientProvider>
+    <PHProvider client={posthog}>
+      <QueryClientProvider client={queryClient}>
+        <NextThemesProvider
+          attribute="class"
+          defaultTheme={initialTheme}
+          enableSystem
+          disableTransitionOnChange
+        >
+          <TooltipProvider>{children}</TooltipProvider>
+        </NextThemesProvider>
+      </QueryClientProvider>
+    </PHProvider>
   );
 }
